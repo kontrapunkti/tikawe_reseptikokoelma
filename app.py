42c36af499488bac0056236573ddcdb6182dde75
Konsta Mönkkönen <230967783+kontrapunkti@users.noreply.github.com>
2026-07-18 23:54:08 +0300

    Added static and templates folders, added basic html-files

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Reseptikokoelma</h1>"
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    if username:
+        return render_template("user.html", username=username)
+    else:
+        return render_template("login.html", error=True)
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
